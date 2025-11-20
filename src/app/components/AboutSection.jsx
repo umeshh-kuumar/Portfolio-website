7947e72cd377eb506dacf1018720915e454ec2ed
UMESH KUMAR <246810umesh@gmail.com>
2025-11-20 18:31:25 +0530

    added certifications in AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -69,20 +69,30 @@ const TAB_DATA = [
     id: "certification",
     content: (
       <div className="space-y-2">
+        <div className="border-l-4 border-lime-500 pl-4">
+          <h4 className="font-semibold text-white">Artificial Intelligence Fundamentals</h4>
+          <p className="text-lime-300 text-sm">IBM</p>
+          <p className="text-gray-400 text-xs">Issued Nov 2025</p>
+        </div>
+        <div className="border-l-4 border-red-500 pl-4">
+          <h4 className="font-semibold text-white">Deloitte Australia - Technology  Job Simulation</h4>
+          <p className="text-red-300 text-sm">forage</p>
+          <p className="text-gray-400 text-xs">Issued June 2025</p>
+        </div>
+        <div className="border-l-4 border-blue-500 pl-4">
+          <h4 className="font-semibold text-white">AWS APAC - Solutions Architecture Job Simulation</h4>
+          <p className="text-blue-300 text-sm">forage</p>
+          <p className="text-gray-400 text-xs">Issued June 2025</p>
+        </div>
+        <div className="border-l-4 border-pink-500 pl-4">
+          <h4 className="font-semibold text-white">Advanced Java Programming</h4>
+          <p className="text-pink-300 text-sm">Anudip</p>
+          <p className="text-gray-400 text-xs">Mar 2024 – Oct 2024</p>
+        </div>
         <div className="border-l-4 border-green-500 pl-4 mb-2">
           <h4 className="font-semibold text-white">JavaScript Certificate</h4>
           <p className="text-green-300 text-sm">Scaler Academy</p>
-          <p className="text-gray-400 text-xs">Issued 2023</p>
-        </div>
-        <div className="border-l-4 border-pink-500 pl-4">
-          <h4 className="font-semibold text-white">Deloitte Australia - Technology  Job Simulation</h4>
-          <p className="text-pink-300 text-sm">forage</p>
-          <p className="text-gray-400 text-xs">Issued 2025</p>
-        </div>
-        <div className="border-l-4 border-pink-500 pl-4">
-          <h4 className="font-semibold text-white">AWS APAC - Solutions Architecture Job Simulation</h4>
-          <p className="text-pink-300 text-sm">forage</p>
-          <p className="text-gray-400 text-xs">Issued 2025</p>
+          <p className="text-gray-400 text-xs">Issued Feb 2024</p>
         </div>
       </div>
     ),
